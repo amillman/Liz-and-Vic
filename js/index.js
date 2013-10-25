729f5972a733f77160485a4710183d12b95ac744
amillman <amillman@ctemc.org>
2013-10-24 20:09:28 -0400

    finished the classes for chat bubbles - both sending and receiving added a fader to replicate more accurately the ios fade in imessage but doesnt work

--- js/index.js
+++ js/index.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
         $("#iphone-wrapper").css({"margin-left": ($(window).width() - $("#iphone-wrapper").width())/2 });
     });
 
+    /*$(window).on('mousewheel DOMMouseScroll',function(e){
+        // To catch up or down scroll:
+        e = e.originalEvent;
+        // delta == 1 = down, delta == -1 = up;
+        var delta = e.wheelDelta > 0 || e.detail < 0 ? 1 : -1;
+
+        if(delta == 1) {
+            $("#chat-box").scrollTop($("#chat-box").scrollTop()-10);
+        }
+        else {
+            $("#chat-box").scrollTop($("#chat-box").scrollTop()+10);
+        }
+    });*/
 });
